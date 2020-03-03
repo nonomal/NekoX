8dc8581741cec5357638b880d8979d7015b8e8a7
世界 <i@nekox.me>
2020-03-04 06:20:43 +0800

    喵

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -19,8 +19,8 @@ public class BuildVars {
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = false;
     public static boolean TON_WALLET_STANDALONE = false;
-    public static int BUILD_VERSION = 1888;
-    public static String BUILD_VERSION_STRING = "5.15.0.19";
+    public static int BUILD_VERSION = 1889;
+    public static String BUILD_VERSION_STRING = "5.15.0.20";
     public static int APP_ID = 1356978; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
     public static String APP_HASH = "becfe4fcb2ddb0ff698bb8daf0260322"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
     public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
